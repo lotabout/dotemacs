0b48366eaadc03f0f70a66803ada8baade230eb3
Zhang Jinzhou <zhang_ji@worksap.co.jp>
2015-08-05 10:50:56 +0800

    [utility] add mw-region-op to operate on region.

--- .emacs.d/elisp/utility-functions.el
+++ .emacs.d/elisp/utility-functions.el
@@ -2,6 +2,22 @@
 (require 's)
 
 ;;; ===========================================================================
+;;; common utilities
+(defun mw-region-op (op)
+  "Call function 'op' on a region of text. The text is replaced with
+   the result of the function call.
+   'op' should take only one argument -- the string of the region"
+  (interactive "X")
+  (let* ((begin (if (use-region-p) (region-beginning) (point-min)))
+	 (end (if (use-region-p) (region-end) (point-max)))
+	 (input-string (buffer-substring-no-properties begin end))
+	 (output-string (funcall op input-string)))
+    (save-excursion
+      (delete-region begin end)
+      (goto-char begin)
+      (insert output-string))))
+
+;;; ===========================================================================
 ;;; nikola utility function
 (defun nikola-set-entry (entry-pair)
   (concat ".. " (car entry-pair) ": " (cdr entry-pair)))
@@ -12,6 +28,7 @@
 	  "\n#+END_COMMENT\n"))
 
 (defun nikola-set-properties (title slug tags category link description)
+  "Prompt and set utilities that is needed by nikola blogging system."
   (interactive "sTitle: \nsSlug: \nsTags(comma seperated): \nsCategory: \nsLink: \nsDescription: ")
   (let ((meta
 	 (nikola-wrap
